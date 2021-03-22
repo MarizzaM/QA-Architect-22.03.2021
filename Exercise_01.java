190e877ce0078b86b7b07abfc6e932839c7998cb
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-03-22 17:48:02 +0200

    Create Exercise_01.java

---
+++ Exercise_01.java
@@ -0,0 +1,12 @@
+package com.app;
+
+import java.time.LocalDateTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDateTime myDate = LocalDateTime.of(1989, 4, 20, 05, 45, 0);
+
+        System.out.println("My birthday: " + myDate);
+    }
+}
