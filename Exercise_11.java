6dc1f4a68478a703fb3d6a0c3ceb377ed737dafc
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-03-22 20:41:48 +0200

    Create Exercise_11.java

---
+++ Exercise_11.java
@@ -0,0 +1,31 @@
+package com.app;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+// (a)
+        Stack<String> str = new Stack<>();
+
+        str.add("One");
+        str.add("Two");
+        str.add("Three");
+// (b)
+        str.remove(2);
+        System.out.println(str);
+// (c)
+        Stack<String> str1 = new Stack<>();
+
+        str1.add("One");
+        str1.add("Two");
+        str1.add("Three");
+
+        System.out.println(str1.peek());
+        System.out.println(str1);
+// (d)
+//      (b) Removing at the object at the top of this stack
+//      (c) Looks at the object at the top of this stack without removing it from the stack.
+
+    }
+}
