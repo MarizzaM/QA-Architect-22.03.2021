425f8c54b4c805db6e946363be1bdc04756016c5
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-03-22 19:01:00 +0200

    Add files via upload

---
+++ Exercise_03/Continent.java
@@ -0,0 +1,27 @@
+package com.app;
+
+public class Continent implements Comparable<Continent>{
+    private String m_name;
+    private double m_size;
+    private int m_population;
+
+    public Continent(String m_name, double m_size, int m_population) {
+        this.m_name = m_name;
+        this.m_size = m_size;
+        this.m_population = m_population;
+    }
+
+    @Override
+    public int compareTo(Continent o) {
+        return this.m_population-o.m_population;
+    }
+
+    @Override
+    public String toString() {
+        return "Continent{" +
+                "m_name='" + m_name + '\'' +
+                ", m_size=" + m_size +
+                ", m_population=" + m_population +
+                '}';
+    }
+}
